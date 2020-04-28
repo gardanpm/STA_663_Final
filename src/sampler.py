18214e43beed828616111870cc424e580d6edf20
cjuracek <therealjuracek@gmail.com>
2020-04-28 13:05:36 -0400

    Further work on creating functions from notebook

--- src/sampler.py
+++ src/sampler.py
@@ -2,7 +2,7 @@ import numpy as np
 from random import choices
 
 
-def LatentDirichletAllocation(iden_to_tokens, K, alpha, beta, niter):
+def LatentDirichletAllocation(iden_to_tokens, K, alpha, niter, beta=0.01):
     ''' Perform collapsed Gibbs sampling to discover latent topics in corpus
 
     :param iden_to_tokens: A dictionary that maps unique identifiers to their contents
@@ -14,18 +14,25 @@ def LatentDirichletAllocation(iden_to_tokens, K, alpha, beta, niter):
     '''
 
     titles = iden_to_tokens.keys()
-    document_word_topics = {title: [] for title in titles}  # Contains the ordered list of topics for each document
+
+    # Contains the ordered list of topics for each document
     # Dict of lists
+    document_word_topics = {title: [] for title in titles}
 
     # Counts of each topic per document (dict of dicts)
     document_topic_counts = {title: dict.fromkeys(range(1, K + 1), 0) for title in titles}
-    word_topic_counts = {word: dict.fromkeys(range(1, K + 1), 0)
-                         for word in unique_words}  # Counts of each topic per word (dict of dicts)
-    total_topic_counts = dict.fromkeys(range(1, K + 1), 0)  # Counts of each topic across all documents
+
+    unique_words = get_unique_words(iden_to_tokens.values())
+    W = len(unique_words)
+    # Counts of each topic per word (dict of dicts)
+    word_topic_counts = {word: dict.fromkeys(range(1, K + 1), 0) for word in unique_words}
+
+    # Counts of each topic across all documents
+    total_topic_counts = dict.fromkeys(range(1, K + 1), 0)
 
     for _ in range(niter):  # One iteration of Gibbs sampler
-        for doc, words in iden_to_tokens.items():  # For every document
-            for i, word in enumerate(words):  # For every word
+        for doc, words in iden_to_tokens.items():
+            for i, word in enumerate(words):
                 densities = np.zeros(K)
                 curr_topic = document_word_topics[doc][i]
                 for k in range(1, K + 1):
@@ -63,3 +70,26 @@ def LatentDirichletAllocation(iden_to_tokens, K, alpha, beta, niter):
 
                 total_topic_counts[curr_topic] -= 1
                 total_topic_counts[new_topic] += 1
+
+
+def compute_phi_estimates(word_topic_counts, total_topic_counts, K, unique_words, beta):
+    W = len(unique_words)
+    phi_matrix = np.zeros((K, W))
+
+    for w, word in enumerate(unique_words):
+        for k in range(1, K + 1):
+            N_wk = word_topic_counts[word][k]
+            N_k = total_topic_counts[k]
+
+            phi_matrix[k - 1, w] = (N_wk + beta) / (N_k + W * beta)
+
+    return phi_matrix
+
+
+def compute_theta_estimates(document_topic_counts, K, alpha):
+
+
+
+def get_unique_words(tokens):
+    unique_words = set().union(*tokens)
+    return list(unique_words)
