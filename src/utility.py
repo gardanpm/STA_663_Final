a84a3b7f6fce14e2279bd2ee2945de3337aa5a90
cjuracek <therealjuracek@gmail.com>
2020-04-30 18:55:11 -0400

    Added docstrings to utility

--- src/utility.py
+++ src/utility.py
@@ -5,9 +5,12 @@ import string
 
 
 def parse_sgm_file(sgm_data):
-    '''
+    """
     Returns a dictionary with titles + articles of an SGM file
-    '''
+
+    :param sgm_data: Data read from an SGM file
+    :return: A dictionary mapping titles to article contents
+    """
 
     soup = BeautifulSoup(sgm_data, features="html5lib")
     texts = soup.find_all('text')
@@ -24,6 +27,13 @@ def parse_sgm_file(sgm_data):
 
 
 def tokenize_doc(doc):
+    """
+    Convert document to lowercase, remove punctuation, and split on whitespace
+
+    :param doc: Document of text (list)
+    :return: Tokens of document
+    """
+    
     doc = doc.lower()
     whitespace = string.whitespace + '\x03'  # End of file char
     trans = str.maketrans(whitespace, ' ' * len(whitespace), string.punctuation)
@@ -32,6 +42,16 @@ def tokenize_doc(doc):
 
 
 def remove_stop_words(tokens, remove_numbers=True, tokens_have_quotes=False, extra_words=None):
+    """
+    Remove top 50 most common stop words, along with numbers, and additional extra words
+
+    :param tokens: List of tokens
+    :param remove_numbers: Should number strings be removed from the tokens?
+    :param tokens_have_quotes: Do the tokens contain quotes?
+    :param extra_words: Additional words that will be removed from tokens
+    :return: List of tokens with appropriate words removed
+    """
+    
     if extra_words is None:
         extra_words = []
 
@@ -48,6 +68,13 @@ def remove_stop_words(tokens, remove_numbers=True, tokens_have_quotes=False, ext
 
 
 def stem_tokens(tokens):
+    """
+    Stem tokens using the Porter stemmer
+
+    :param tokens: List of tokens
+    :return: A stemmed list of the same tokens
+    """
+    
     ps = PorterStemmer()
     tokens_stemmed = [ps.stem(token) for token in tokens]
     return tokens_stemmed
